f820cadd6198548fd125b308f7fd834d9f2ca4d4
Adrià <adriablancafort@gmail.com>
2025-10-20 00:03:06 +0200

    shadcn init

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <div>Hello</div>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
