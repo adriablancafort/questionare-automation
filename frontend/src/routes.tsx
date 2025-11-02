5f5d7996e7473825246f2e1ad4f78413ea561f0a
Adrià <adriablancafort@gmail.com>
2025-11-02 20:40:41 +0100

    add shadcn layout

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router"
+import DashboardLayout from "@/pages/layout.tsx"
 import Home from "@/pages/index.tsx"
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<DashboardLayout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
